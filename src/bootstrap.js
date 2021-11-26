04e0da790450018959895db9d9467e42734c51fa
CorentinLIRIS <corentin.chignin@hotmail.fr>
2021-11-26 08:44:41 +0100

    Add google map pins

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -38,6 +38,8 @@ udviz.Components.SystemUtils.File.loadJSON(
     view3D.getScene()
   );
 
+  const scene3D = view3D.getScene();
+
   //Lighting
   const { directionalLight, ambientLight } =
   udviz.Game.Shared.Components.THREEUtils.addLights(view3D.getScene());
@@ -52,7 +54,7 @@ udviz.Components.SystemUtils.File.loadJSON(
   const center = view3D.getExtent().center();
 
   const iframe_Episode_1 = document.createElement('iframe');
-  iframe_Episode_1.src = 'https://www.interland.info/wp-content/uploads/2018/03/VM-vallee-de-la-Chimie-7.jpg';
+  iframe_Episode_1.src = 'https://visite.interfora-connect.com/Visite/index.html';
   const billboard_Episode_1 = new udviz.Widgets.Billboard(
     iframe_Episode_1,
     new udviz.Game.Shared.Components.THREEUtils.Transform(
@@ -64,7 +66,7 @@ udviz.Components.SystemUtils.File.loadJSON(
   );
 
   const iframe_Episode_2 = document.createElement('iframe');
-  iframe_Episode_2.src = 'https://ud-viz.vcityliris.data.alpha.grandlyon.com/examples/AllWidget/example.html';
+  iframe_Episode_2.src = 'https://visite.interfora-connect.com/Visite/index.html';
   const billboard_Episode_2 = new udviz.Widgets.Billboard(
     iframe_Episode_2,
     new udviz.Game.Shared.Components.THREEUtils.Transform(
@@ -77,8 +79,55 @@ udviz.Components.SystemUtils.File.loadJSON(
 
   view3D.initCSS3D();
   
-  view3D.appendBillboard(billboard_Episode_1);
-  view3D.appendBillboard(billboard_Episode_2);
-  console.log(billboard_Episode_1.getCss3DObject());
+  // view3D.appendBillboard(billboard_Episode_1);
+  // view3D.appendBillboard(billboard_Episode_2);
+  
+  //Pins episodes
+  const pinsTexture = new udviz.THREE.TextureLoader().load( '../assets/img/1200px-Google_Maps_pin.svg.png' );
+  const pinsMaterial = new udviz.THREE.SpriteMaterial( { map: pinsTexture } );
+  const pinsSprite = new udviz.THREE.Sprite( pinsMaterial );
+  pinsSprite.position.set(center.x + 800, center.y + 800, 600);
+  pinsSprite.scale.set(200,300,1);
+  pinsSprite.updateMatrixWorld();
+  scene3D.add(pinsSprite);
+
+  const geometry = new udviz.THREE.BoxGeometry( 300, 300, 300 );
+  const material = new udviz.THREE.MeshBasicMaterial( {color: 0x00ff00} );
+  const cube = new udviz.THREE.Mesh( geometry, material );
+  cube.position.set(center.x + 800, center.y + 800, 600);
+  cube.updateMatrixWorld();
+
+  document.addEventListener( 'click', onDocumentMouseClick );
+
+  const episode = document.createElement('div');
+  episode.id = 'Episode 1';
+  episode.src = 'https://visite.interfora-connect.com/Visite/index.html';
+  episode.style.width = 80 * 30 + 'px';
+  episode.style.height = 80 * 25 + 'px';
+
+  
+
+  //Select sprites
+  function onDocumentMouseClick( event ) {    
+    event.preventDefault();
+    let mouse3D = new udviz.THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1,   
+                            -( event.clientY / window.innerHeight ) * 2 + 1,  
+                            0.5 );     
+                                            
+    let raycaster =  new udviz.THREE.Raycaster();                                        
+    raycaster.setFromCamera( mouse3D, view3D.getCamera());
+
+    //All objects hits
+    let intersects = raycaster.intersectObjects( view3D.getScene().children ); 
+    console.log(intersects);
+    if ( intersects.length > 0 ) {
+        intersects[ 0 ].object.scale.set(intersects[ 0 ].object.scale.x + 50, intersects[ 0 ].object.scale.y + 50,intersects[ 0 ].object.scale.z + 50);
+        
+        console.log(episode);
+        intersects[ 0 ].object.updateMatrixWorld();
+    }
+}
+
+  console.log(scene3D);
   
 });
