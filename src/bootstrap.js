3b39ee1fce8491b654ae89befcb855d94b2a8140
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-03-03 11:17:39 +0100

    Rset projection to 3946 but elevation layer destry ud-viz

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -108,12 +108,6 @@ udviz.Components.SystemUtils.File.loadJSON(
     //new udviz.Templates.AllWidget().addModuleView('layerChoice', layerChoice);
 
     let color = new udviz.THREE.Color();
-    
-    view3D.getScene().children[0].position.set(view3D.getScene().children[0].position.x,
-      view3D.getScene().children[0].position.y + 50,
-      view3D.getScene().children[0].position.z + 160);
-
-    console.log(view3D.getScene().children[0].position);
 
     function colorSurfaceBatiments() {
       return color.set(0x008000);
